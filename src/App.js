af3977cfec4513b50c4afc2456de5adaeb1458f5
Federico Vicente <fedevicente@gmail.com>
2020-07-31 20:47:36 -0300

    Ex 2 Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand  } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand}  from 'reactstrap'
 import './App.css';
+import Menu from "./components/MenuComponent";
+import {DISHES} from './shared/dishes';
+ 
+class App extends Component{
+  
+  constructor(props){
+    super(props);
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-          Juan Carlos
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+    this.state = {
+      dishes: DISHES
+    };
+  }
+ 
+  render() {
+    return (
+      <div>
+        <Navbar dark color='primary'>
+          <div className = 'container'>
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand> 
+          </div>
+        </Navbar>
+        <Menu dishes= {this.state.dishes}/>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
+ 
+
